f16a43b3771922524e857118a9e9c7c14f1c6e30
Pawel Licznerski <pawel.licznerski3@gmail.com>
2017-10-05 18:47:44 +0200

    structure2

--- src/components/follow-app/main-profile/__main-profile__details/main-profile__details.js
+++ src/components/follow-app/main-profile/__main-profile__details/main-profile__details.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import pic from './components/follow-app/main-profile/__main-profile__details/Rick-astley.jpg';
+// import Pic from './Rick-astley.jpg';
+
 
 class Details extends Component {
   render() {
     return (
         <div className="main-profile__details">
-          <img className="main-profile__details__img" alt="Rick" src={pic}></img>
+          <div className="main-profile__details__img"></div>
           <div className="main-profile__details__text">
             <h1 className="main-profile__details__text__name">Rick Ashtley</h1>
             <span className="main-profile__details__text__address">Swarzędz, Poland</span>
