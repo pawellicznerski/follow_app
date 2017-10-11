841021a467608dbc9ff5e9bd5f7ccd99b638d9f0
Pawel Licznerski <pawel.licznerski3@gmail.com>
2017-10-11 22:28:57 +0200

    adding redux rendering comments

--- src/components/follow-app/comments/comments.js
+++ src/components/follow-app/comments/comments.js
@@ -7,17 +7,30 @@ class Comments extends Component {
     this.state={
       showComments:true,
       commentsCounter:"(23)",
+      value:''
     }
   }
+  //watches value change in input
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  //watches value change in input
+  handleSubmit(event) {
+    this.props.addComment()
+    this.setState({value: ''});
+  }
+
   render() {
     const {comments, posts}= this.props;
     return (
       <div className="comments">
         <button className="comments__toggle-comments">{this.state.showComments?"Hide ":"Show "}comments{comments[posts[0].code]?`(${comments[posts[0].code].length})`:"(0)"}</button>
         <CommentsGrid {...this.props}></CommentsGrid>
-        <div className="comments__input-cnt">
-          <input type="text" className="comments__input" placeholder="Add a comment"></input>
-        </div>
+        <form className="comments__input-cnt" onSubmit={this.handleSubmit} onBlur={this.handleBlur}>
+          <input value='' type="text" className="comments__input" placeholder="Add a comment" onChange={this.handleChange} ></input>
+
+        </form>
       </div>
     );
   }
