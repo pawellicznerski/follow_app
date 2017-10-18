c1af25a95d9026b38fe8b75ef5c419a8df676ce5
Pawel Licznerski <pawel.licznerski3@gmail.com>
2017-10-18 22:11:57 +0200

    adding redux rendering comments

--- src/components/follow-app/comments/comments.js
+++ src/components/follow-app/comments/comments.js
@@ -7,50 +7,79 @@ class Comments extends Component {
     this.state={
       showComments:true,
       commentsCounter:"(23)",
-      value:''
+      comment:'',
+      user:''
     };
-    this.dupa=this.dupa.bind(this)
+    this.handleInputChange=this.handleInputChange.bind(this)
+    this.handleSubmit=this.handleSubmit.bind(this)
+    this.toggleComments=this.toggleComments.bind(this)
   }
-componentDidMount(){
-  console.log(this);
-  console.log(this.state.showComments);
-}// tu jest fit
+// componentDidMount(){
+//   console.log(this);
+//   console.log(this.state.showComments);
+// }// tu jest fit
 
-  dupa(){
+  toggleComments(){
     console.log(this);
     console.log(this.state);
   }// tu jest undefined
 
   //watches value change in input
-  handleChange(event) {
-    this.setState({value: event.target.value});
-  }
+  handleSubmit(e) {
+    // e.preventDefault();
+    // console.log(this.state.user);
+    // console.log(this.state.comment);
+    //
+    // console.log(this);
+    this.props.addComment(145,this.state.user,this.state.comment);
 
-  //watches value change in input
-  handleSubmit() {
+    this.setState({
+      user: '',
+      comment:''
+    });
 
-    this.props.addComment(1,'Krzyś',"this.state.value")
-    this.setState({value: ''});
   }
+  handleInputChange(e){
+    // e.preventDefault();
+    // console.log(e.target);
+    const target = e.target;
+    const value = target.value;
+    // console.log(value);
+    const name = target.name;
+    // console.log(name);
+    this.setState({
+       [name]: value,
+     });
+    //  console.log(this.state.user);
+    //  console.log(this.state.comment);
+  } //end of handleInputChange
 
   render() {
     const {comments, posts}= this.props;
     return (
       <div className="comments">
         <button
-          onClick={this.dupa.bind(this)} className="comments__toggle-comments">{this.state.showComments?"Hide ":"Show "}comments{comments[posts[0].code]?`(${comments[posts[0].code].length})`:"(0)"} </button>
+          onClick={this.toggleComments} className="comments__toggle-comments">{this.state.showComments?"Hide ":"Show "}comments{comments[posts[0].code]?`(${comments[posts[0].code].length})`:"(0)"} </button>
         <CommentsGrid {...this.props}></CommentsGrid>
         <form
           className="comments__input-cnt"
           onSubmit={this.handleSubmit}
           >
           <input
-            value=''
             type="text"
             className="comments__input"
             placeholder="Add a comment"
-            onChange={this.handleChange.bind(this)}
-          ></input>
+            onChange={this.handleInputChange}
+            name="comment"
+          />
+          <input
+            type="text"
+            className="comments__input"
+            placeholder="Your name"
+            onChange={this.handleInputChange}
+            name="user"
+          />
+        <input type="submit" value="submit"></input>
         </form>
       </div>
     );
